29bcd60f2c89d2b55ad7ea1c0822e3285e18c575
BAHADIRSENGUN <bahadirsngun@gmail.com>
2023-03-31 23:01:45 +0300

    Sayfalar, Tasarımlar ve geçişler oluşturuldu ayrıca butonlar, textfield, searchbar ve tablo viewler de sayfalara bağlandı.

--- myDream/Login Page/LoginPageViewController.swift
+++ myDream/Login Page/LoginPageViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class LoginPageViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var loginPageEmailTextField: UITextField!
+    
+    @IBOutlet weak var loginPagePasswordTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +25,11 @@ class LoginPageViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginPageLoginButton(_ sender: Any) {
     }
-    */
+    
+    
+    
+    
 
 }
--- myDream/TableViewVeriViewController.swift
+++ myDream/TableViewVeriViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class TableViewVeriViewController: UIViewController {
 
+    
+    @IBOutlet weak var sembolSearchBar: UISearchBar!
+    
+    @IBOutlet weak var sembolTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+    
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
