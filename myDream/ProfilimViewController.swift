0db1613392c6ac91abbe0c7fb09efe975c46eeb6
Bircan Sezgin <bircansezgin@outlook.com.tr>
2023-04-09 17:42:53 +0300

    menüye foto ekledim. Profilim sayafini arka planı değiştirdim. profilim sekmesine Hesabimi sil butonu ekledim.

--- myDream/ProfilimViewController.swift
+++ myDream/ProfilimViewController.swift
@@ -30,6 +30,38 @@ class ProfilimViewController: UIViewController {
     }
     
     
+    @IBAction func hesabiSil(_ sender: Any) {
+        let user = Auth.auth().currentUser
+         
+         let alertController = UIAlertController(title: "Hesap Silme", message: "Hesabınızı silmek istediğinize emin misiniz?", preferredStyle: .alert)
+         let evetAction = UIAlertAction(title: "Evet", style: .destructive) { (action) in
+             user?.delete { error in
+                 if let error = error {
+                     print("Hesap silme hatası: \(error.localizedDescription)")
+                     // Hesap silme hatası olduğunda kullanıcıya hata mesajını gösterebilirsiniz.
+                     // Örneğin bir alert ile kullanıcıya hata mesajını göstermek için aşağıdaki kodu kullanabilirsiniz:
+                     let errorAlertController = UIAlertController(title: "Hesap Silme Hatası", message: error.localizedDescription, preferredStyle: .alert)
+                     let okAction = UIAlertAction(title: "Tamam", style: .default, handler: nil)
+                     errorAlertController.addAction(okAction)
+                     self.present(errorAlertController, animated: true, completion: nil)
+                 } else {
+                     print("Hesap başarıyla silindi.")
+                     exit(-1)
+                     // Hesap başarıyla silindiyse gerekli işlemleri yapabilirsiniz, örneğin kullanıcıyı uygulamadan çıkartabilirsiniz.
+                 }
+             }
+         }
+        let hayirAction = UIAlertAction(title: "Hayır", style: .default) { UIAlertAction in
+                //
+        }
+         alertController.addAction(evetAction)
+         alertController.addAction(hayirAction)
+         self.present(alertController, animated: true, completion: nil)
+       
+    }
+    
+    
+    
     func cikisAler(title:String, message:String){
         let uyari = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         let noButton = UIAlertAction(title: "No", style: .default)
@@ -51,5 +83,5 @@ class ProfilimViewController: UIViewController {
         self.present(uyari, animated: true)
     }
     
-
 }
+
